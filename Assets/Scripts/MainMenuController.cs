d9ebf89dc90b569addbcb97cd2e3251914f96165
Henry Oliver <henry983615@gmail.com>
2020-05-05 18:27:14 +1200

    googleservices?

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,6 +9,7 @@ public class MainMenuController : MonoBehaviour
 {
 
     private MillionairBaby iap;
+    private bool userLogginedIntoGoogle = false;
 
     private void Start()
     {
@@ -14,6 +17,53 @@ public class MainMenuController : MonoBehaviour
         AdTime.Initialize();
         //Start Money Machine
         iap = GetComponent<MillionairBaby>();
+
+
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+        PlayGamesPlatform.Instance.Authenticate(SignInInteractivity.CanPromptOnce, OnAuthenticated);
+
+        ////Connect to Google Services
+        //PlayGamesClientConfiguration.Builder builder = new PlayGamesClientConfiguration.Builder();
+        //PlayGamesPlatform.InitializeInstance(builder.Build());
+        ////Debugger
+        ////#if UNITY_EDITOR
+        //PlayGamesPlatform.DebugLogEnabled = true;
+        ////#endif
+        //PlayGamesPlatform.Activate();
+
+    }
+
+    private void OnAuthenticated(SignInStatus result)
+    {
+        switch (result)
+        {
+            case SignInStatus.Success:
+                Debug.Log("GPG sign in successful");
+                break;
+            default:
+                Debug.Log($"GPG sign in failed: {result}");
+                break;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //if (!userLogginedIntoGoogle)
+        //{
+        //    Social.localUser.Authenticate((bool success, string errorMsg) =>
+        //    {
+        //        if (success)
+        //        {
+        //            Debug.Log("Auth success to google services");
+        //            userLogginedIntoGoogle = true;
+        //        }
+        //        else
+        //        {
+        //            Debug.Log($"Failed to Auth to google services [{errorMsg}]");
+        //        }
+        //    });
+        //}
     }
 
     public void buyNoAds()
